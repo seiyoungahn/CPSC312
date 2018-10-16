7b653e0050b7cb109488fb06e9f73cc94d17886b
Seiyoung Ahn <sedddong@gmail.com>
2018-10-15 22:58:36 -0700

    Add convertScore method

--- Parser.hs
+++ Parser.hs
@@ -2,26 +2,24 @@ module Parser where
 
 import System.IO
 
--- foo lst = [(quadgram,score) | (quadgram:" ":score:"\n") <- lst] 
+-- read quadgrams textfile and convert it into list of tuples. both quadgrams and score are represented as string
+parseQuadgrams :: [Char] -> [([Char], [Char])] -> Bool -> [([Char], [Char])]
+parseQuadgrams [] acc quadgram = acc
+parseQuadgrams lst acc quadgram =
+    if (h == ' ') then (parseQuadgrams t acc (not quadgram))
+    else if (h == '\n') then (parseQuadgrams t (("", "") : acc) (not quadgram))
+    else if (quadgram) then (parseQuadgrams t ((quad ++ [h] , score) : (tail acc)) quadgram)
+    else (parseQuadgrams t ((quad , score ++ [h]) : (tail acc)) quadgram)
+        where
+        h = head lst
+        t = tail lst
+        (quad, score) = head acc
 
-consi x y = y : x 
+-- convert parsed list of tuples so that the score becomes an integer
+convertScore :: [([Char], [Char])] -> [([Char], Integer)]
+convertScore lst = [ (quadgrams, read score :: Integer) | (quadgrams, score) <- lst ]
 
--- first call -> foo file [("", "")] True
-foo :: [Char] -> [([Char], [Char])] -> Bool -> [([Char], [Char])]
-foo [] acc quadgram = acc
-foo lst acc quadgram =
-    if (h == ' ') then (foo t acc (not quadgram))
-    else if (h == '\n') then (foo t (("", "") : acc) (not quadgram))
-    else if (quadgram) then (foo t ((quad ++ [h] , score) : (tail acc)) quadgram)
-    else (foo t ((quad , score ++ [h]) : (tail acc)) quadgram)
-    where
-    h = head lst
-    t = tail lst
-    (quad, score) = head acc
-
-
-run n = 
+parse n = 
     do
         file <- readFile "quadgrams.txt"
-        -- return file
-        return (take n (foo file [("", "")] True))
+        return (convertScore (take n (parseQuadgrams file [("", "")] True)))
